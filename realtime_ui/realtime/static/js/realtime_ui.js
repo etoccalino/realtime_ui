30fb66c4ef86a4c064c4d1ba659610cad4fca657
etoccalino <elvio.toccalino@cultura.gov.ar>
2012-11-06 17:01:50 -0300

    cleaned up the client code. implemented a handshake to clarify.

--- realtime_ui/realtime/static/js/realtime_ui.js
+++ realtime_ui/realtime/static/js/realtime_ui.js
@@ -1,6 +1,6 @@
-var update_button = function(btn, is_active){
-    console.log('updating button to state:' + is_active);
+// Helper functions related to DOM manipulation for the UI.
 
+var update_button = function(btn, is_active){
     if (is_active) {
         btn.removeClass('active');
     }
@@ -16,18 +16,32 @@ var update_ui = function(state){
 
 /* ------------------------------------------------------------------- */
 
+// Event handlers for the three-step handshake.
+//
+// First and third part are take place in the clien.
+// Second part takes part in the server.
+// There's also a "zero" part, which is produced automatically by
+// the socke.io mechanism (we pick up at the "connect" event).
+
 socket.on('connect', function () {
-    alert('connect event received...');
     // Connection has been established with server.
     // Inform that this client is ready to receive current state of UI.
+    //
+    // First part of a silly handshake; I suppose I'll learn to avoid it.
     socket.emit('ready');
 });
 
 socket.on('initial', function (state){
     // Receive the current state of the UI, after connecting.
+    //
+    // Second part of a silly handshake; I suppose I'll learn to avoid it.
     alert('Updating your UI...');
     update_ui(state);
 });
+
+/* ------------------------------------------------------------------- */
+
+// Event handlers.
 
 socket.on('refresh', function (state){
     alert('UI has been refreshed... all is lost...');
@@ -35,11 +49,13 @@ socket.on('refresh', function (state){
 });
 
 socket.on('remote click', function(button_state){
-    console.log('remote state:' + button_state);
     update_button($('#rt_button'), button_state.rt_button);
 })
 
-// send messages
+/* ------------------------------------------------------------------- */
+
+// Event signals.
+
 $(function () {
     $('#rt_button').on('click', function(){
         socket.emit('click');
