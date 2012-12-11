71824e1dafaaa859a3b1c52d7381d1ac02f8b68a
Elvio Toccalino <etoccalino@creativa77.com.ar>
2012-12-11 14:59:48 -0300

    serve static files from the devel server.

--- realtime_ui/urls.py
+++ realtime_ui/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
     url(r'^', include('realtime_ui.realtime.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
